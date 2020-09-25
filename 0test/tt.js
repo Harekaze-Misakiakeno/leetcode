8c06962d17dabb0ae3b1d5531c81e3e669a2208f
Harekaze-Misakiakeno <Efreet_Itsukakotori@hotmail.com>
2020-09-25 20:59:06 +0800

    1

--- 0test/tt.js
+++ 0test/tt.js
@@ -1,6 +1,12 @@
-// import test from './test' 
-var value = 1000
-var a = require('./test.js')
-// console.log(value)
-var x = new a(1 ,2 )
-x.printSum()
+// // import test from './test' 
+// var value = 1000
+// var a = require('./test.js')
+// // console.log(value)
+// var x = new a(1 ,2 )
+// x.printSum()
+
+let res = []
+for(let i = 2020; i < 2033; i++){
+  res.push(i + 0.12)
+}
+console.log(res.toString())
