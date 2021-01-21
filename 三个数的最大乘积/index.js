1963fe4a75d9e6b9396decd4df7fa3c599a6688c
Harekaze-Misakiakeno <Efreet_Itsukakotori@hotmail.com>
2021-01-21 22:32:16 +0800

    1

---
+++ 三个数的最大乘积/index.js
@@ -0,0 +1,33 @@
+// 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+
+// 示例 1:
+
+// 输入: [1,2,3]
+// 输出: 6
+// 示例 2:
+
+// 输入: [1,2,3,4]
+// 输出: 24
+// 注意:
+
+// 给定的整型数组长度范围是[3,104]，数组中所有的元素范围是[-1000, 1000]。
+// 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/maximum-product-of-three-numbers
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * 思路都是最小两个和最大三个
+ * 排序或者线性扫描
+ */
+var maximumProduct = function(nums) {
+  nums.sort((a,b)=>{return a-b})
+  let a = nums[nums.length-3] * nums[nums.length-2] * nums[nums.length-1]
+  let b = nums[0] * nums[1] * nums[nums.length-1]
+  return a > b ? a : b
+};
+console.log(maximumProduct([1,2,3,4]))
